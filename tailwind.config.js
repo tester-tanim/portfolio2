a1016629e2b200165551e46c8de65fb1164aa73a
Ishtiaque Ahmed Tanim <68701811+TANIMBD2@users.noreply.github.com>
2023-08-03 16:37:03 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  darkMode:"class",
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      spacing:{
+        "big":"48rem"
+      }
+    },
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
+    },
+    fontFamily:{
+      nunito:['Nunito', 'sans-serif']
+    }
+  },
+}
